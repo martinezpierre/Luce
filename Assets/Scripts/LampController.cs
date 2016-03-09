3084ff483035f4028f61a909705031e64a55dcd9
unknown <pi.martinez@J001-0135-05.etudiant.rubika-edu.com.local>
2016-03-10 00:54:20 +0100

    merge LampController

--- Assets/Scripts/LampController.cs
+++ Assets/Scripts/LampController.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 [RequireComponent(typeof(Light))]
-public class LampController : MonoBehaviour {
+public class LampController : MonoBehaviour
+{
 
     [HideInInspector]
     public bool lighted = false;
@@ -10,7 +11,8 @@ public class LampController : MonoBehaviour {
     float elapsedT, intensity, lightModifier = 0.5f, random;
     Light halo;
 
-    void Awake() {
+    void Awake()
+    {
         halo = GetComponent<Light>();
         intensity = halo.intensity;
         halo.intensity = 0;
@@ -19,25 +21,44 @@ public class LampController : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player" && !lighted)
+        if (other.tag == "Player")
         {
-            halo.enabled = true;
-            LampManager.Instance.NewLampLighted(gameObject);
-            lighted = true;
+
+            other.GetComponentInParent<Burst>().canReload = true;
+
+            if (!lighted)
+            {
+                halo.enabled = true;
+                LampManager.Instance.NewLampLighted(gameObject);
+                lighted = true;
+            }
         }
     }
 
-    void Update() {
-        if (lighted) {
-            if (elapsedT < fadeDuration) {
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponentInParent<Burst>().canReload = false;
+        }
+    }
+
+    void Update()
+    {
+        if (lighted)
+        {
+            if (elapsedT < fadeDuration)
+            {
                 GetComponent<Renderer>().material.SetFloat("_blend", Mathf.Lerp(0, 1, elapsedT / fadeDuration));
                 halo.intensity = Mathf.Lerp(0, intensity, elapsedT / fadeDuration);
                 elapsedT += Time.deltaTime;
                 if (Mathf.Abs(elapsedT / fadeDuration - 1) < 0.05) GetComponent<Renderer>().material.SetFloat("_blend", 1);
-            } else {
+            }
+            else
+            {
                 float noise = Mathf.PerlinNoise(random, Time.time);
-                halo.intensity = Mathf.Lerp(intensity-lightModifier, intensity+lightModifier, noise);
-                GetComponent<Renderer>().material.SetFloat("_blend", Mathf.Lerp(1-lightModifier, 1, noise));
+                halo.intensity = Mathf.Lerp(intensity - lightModifier, intensity + lightModifier, noise);
+                GetComponent<Renderer>().material.SetFloat("_blend", Mathf.Lerp(1 - lightModifier, 1, noise));
             }
         }
     }
