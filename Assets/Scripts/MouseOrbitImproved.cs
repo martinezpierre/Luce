00261e6434cbe808654c6a07126d303ce554893b
pierre martinez <pierre martinez>
2016-03-20 16:37:16 +0100

    some camera changes

--- Assets/Scripts/MouseOrbitImproved.cs
+++ Assets/Scripts/MouseOrbitImproved.cs
@@ -6,6 +6,7 @@ public class MouseOrbitImproved : MonoBehaviour
 {
 
     public Transform target;
+    public Transform zoomTarget;
     public Material corpMaterial;
 
     public float distance = 5.0f;
@@ -69,7 +70,7 @@ public class MouseOrbitImproved : MonoBehaviour
             pC.aim.enabled = false;
         }
 
-        if (aiming || PlayerController.fpsView) return;
+        if (aiming || PlayerController.fpsView /*|| !transform.parent*/) return;
 
         x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
         if (target.GetComponent<PlayerController>().jumping && target.GetComponent<PlayerController>().somethingFoward)
@@ -94,7 +95,7 @@ public class MouseOrbitImproved : MonoBehaviour
 
         RaycastHit hit;
 
-        if (Physics.Linecast(target.position, transform.position, out hit) && hit.transform.tag == "Building")
+        if (Physics.Linecast(zoomTarget.position, transform.position, out hit) && hit.transform.gameObject != zoomTarget)
         {
             distance -= .25f; //hit.distance;
         }
@@ -116,7 +117,7 @@ public class MouseOrbitImproved : MonoBehaviour
 
     public void startCorrection()
     {
-        StartCoroutine(correctCameraAngle());
+        //StartCoroutine(correctCameraAngle());
     }
 
     IEnumerator correctCameraAngle()
@@ -142,79 +143,3 @@ public class MouseOrbitImproved : MonoBehaviour
         return Mathf.Clamp(angle, min, max);
     }
 }
-
-/*using UnityEngine;
-using System.Collections;
-
-[AddComponentMenu("Camera-Control/Mouse Orbit with zoom")]
-public class MouseOrbitImproved : MonoBehaviour
-{
-
-    public Transform target;
-    public float distance = 5.0f;
-    public float xSpeed = 120.0f;
-    public float ySpeed = 120.0f;
-
-    public float yMinLimit = -20f;
-    public float yMaxLimit = 80f;
-
-    public float distanceMin = .5f;
-    public float distanceMax = 15f;
-
-    private Rigidbody rigidbody;
-
-    float x = 0.0f;
-    float y = 0.0f;
-
-    // Use this for initialization
-    void Start()
-    {
-        Vector3 angles = transform.localEulerAngles;
-        x = angles.y;
-        y = angles.x;
-
-        rigidbody = GetComponent<Rigidbody>();
-
-        // Make the rigid body not change rotation
-        if (rigidbody != null)
-        {
-            rigidbody.freezeRotation = true;
-        }
-    }
-
-    void LateUpdate()
-    {
-        if (target && !Input.GetMouseButton(1))
-        {
-            //x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
-            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
-
-            y = ClampAngle(y, yMinLimit, yMaxLimit);
-
-            Quaternion rotation = Quaternion.Euler(y, x, 0);
-
-            distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
-
-            RaycastHit hit;
-            if (Physics.Linecast(target.position, transform.position, out hit) && hit.transform.tag == "Building")
-            {
-                distance -= hit.distance/2;
-            }
-
-            Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
-            Vector3 position = rotation * negDistance + target.position;
-
-            transform.localRotation = rotation;
-            transform.localPosition = position;
-        }
-    }
-
-    public static float ClampAngle(float angle, float min, float max)
-    {
-        if (angle < -360F)
-            angle += 360F;
-        if (angle > 360F)
-            angle -= 360F;
-        return Mathf.Clamp(angle, min, max);
-    }
-}*/
